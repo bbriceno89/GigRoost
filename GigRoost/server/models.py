8855af29b6adb11d8802f5f6027a71c7c390969a
tgartland89 <thomas.gartland@gmail.com>
2023-07-17 11:22:40 -0600

    updated models.py

--- GigRoost/server/models.py
+++ GigRoost/server/models.py
@@ -1,14 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 class Accommodation(Base):
     __tablename__ = 'accommodation'
 
     accommodation_id = Column(Integer, primary_key=True)
-    host_id = Column(Integer, ForeignKey('user.user_id'))
+    host_id = Column(Integer, ForeignKey('users.id'))
     location = Column(String)
     beds = Column(Integer)
     baths = Column(Float)
