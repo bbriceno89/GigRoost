38d87723261b8b70eeeb08b3de75295be0dc95be
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-17 14:16:32 -0600

    fix models imports

--- GigRoost/server/app.py
+++ GigRoost/server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, abort, make_response, session
 from flask_restful import Resource
-from models import User
+from models import User, Show, Accommodation, ArtistBooking, Review
 from config import app, db, api
 
 
@@ -20,13 +20,27 @@ class Signup(Resource):
             return make_response(new_user.to_dict(), 201)
         except:
             return make_response({'errors':['validation errors']}, 400)
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter(User.username == data['username']).first()
+        if user:
+            if user.authenticate(data['password']):
+                session['user_id'] = user.id
+                return user.to_dict(), 201
+            else:
+                return {"error": "Invalid Password"}, 401
+        else:
+            return {"error": "Invalid Username"}, 401
+
         
 api.add_resource(Signup, '/signup')
 
 # Route to get all reviews
 @app.route('/reviews', methods=['GET'])
 def get_all_reviews():
-    reviews = models.Review.query.all()
+    reviews = Review.query.all()
     return jsonify([review.__dict__ for review in reviews])
 
 # Route to Post a review
@@ -34,7 +48,7 @@ def get_all_reviews():
 def post_review():
     if not request.json:
         abort(400)
-    review = models.Review(
+    review = Review(
         writer_id=request.json['writer_id'],
         accomodations_id=request.json['accomodations_id'],
         rating=request.json['rating'],
@@ -47,7 +61,7 @@ def post_review():
 #Route to delete a review
 @app.route('/reviews/<int:review_id>', methods=['DELETE'])
 def delete_review(review_id):
-    review = models.Review.query.filter_by(review_id=review_id).first()
+    review = Review.query.filter_by(review_id=review_id).first()
     if not review:
         abort(404)
     db.session.delete(review)
@@ -58,13 +72,13 @@ def delete_review(review_id):
 # Route to get all artist bookings
 @app.route('/artist_bookings', methods=['GET'])
 def get_all_artist_bookings():
-    artist_bookings = models.ArtistBooking.query.all()
+    artist_bookings = ArtistBooking.query.all()
     return jsonify([a.__dict__ for a in artist_bookings])
 
 # Route to get all shows
 @app.route('/shows', methods=['GET'])
 def get_all_shows():
-    shows = models.Show.query.all()
+    shows = Show.query.all()
     return jsonify([show.__dict__ for show in shows])
 
 
@@ -74,7 +88,7 @@ def get_all_shows():
 # Route to get all accommodations
 @app.route('/accommodations', methods=['GET'])
 def get_all_accommodations():
-    accommodations = models.Accommodation.query.all()
+    accommodations = Accommodation.query.all()
     return jsonify([a.__dict__ for a in accommodations])
 
 # Route to create a new accommodation
@@ -94,7 +108,7 @@ def create_accommodation():
     if not location or not beds or not baths or not sq_ft or not description or not availability_dates:
         return jsonify({'error': 'Please provide all required fields.'}), 400
 
-    accommodation = models.Accommodation(
+    accommodation = Accommodation(
         location=location,
         beds=beds,
         baths=baths,
@@ -111,7 +125,7 @@ def create_accommodation():
 # Route to get a specific accommodation
 @app.route('/accommodations/<int:accommodation_id>', methods=['GET'])
 def get_accommodation(accommodation_id):
-    accommodation = models.Accommodation.query.get(accommodation_id)
+    accommodation = Accommodation.query.get(accommodation_id)
 
     if not accommodation:
         return jsonify({'error': 'Accommodation not found.'}), 404
@@ -121,7 +135,7 @@ def get_accommodation(accommodation_id):
 # Route to partially update an existing accommodation using PATCH
 @app.route('/accommodations/<int:accommodation_id>', methods=['PATCH'])
 def patch_accommodation(accommodation_id):
-    accommodation = models.Accommodation.query.get(accommodation_id)
+    accommodation = Accommodation.query.get(accommodation_id)
 
     if not accommodation:
         return jsonify({'error': 'Accommodation not found.'}), 404
@@ -139,7 +153,7 @@ def patch_accommodation(accommodation_id):
 # Route to delete an accommodation
 @app.route('/accommodations/<int:accommodation_id>', methods=['DELETE'])
 def delete_accommodation(accommodation_id):
-    accommodation = models.Accommodation.query.get(accommodation_id)
+    accommodation = Accommodation.query.get(accommodation_id)
 
     if not accommodation:
         return jsonify({'error': 'Accommodation not found.'}), 404
