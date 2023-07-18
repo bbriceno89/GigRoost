50cbe9d93981f8224891acc08b9bdd2ef6ab2951
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-18 16:47:42 -0600

    handle changes on login form

--- GigRoost/client/src/components/Login.jsx
+++ GigRoost/client/src/components/Login.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login() {
+    const [formData, setFormData] = useState({})
+
+    function handleChange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormData({...formData, [name]: value});
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formData)
+    }
+
+
   return (
     <>
       <p>Login</p>
       <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log("submitted");
-        }}
-      >
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+        value = {formData}>
         <input type="text" placeholder="Username" name="username" />
         <input type="text" placeholder="Password" name="password" />
         <button type="submit">Submit</button>
