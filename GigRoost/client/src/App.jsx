1d289057e33a80ebf4353c594c2136a821d7b7d5
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-19 16:20:22 -0600

    conditionally render components based on user state

--- GigRoost/client/src/App.jsx
+++ GigRoost/client/src/App.jsx
@@ -3,15 +3,17 @@ import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import CardList from "./components/CardList"; 
 import { UserContext } from "./components/context/UserContext";
+import { Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const { user, setUser } = useContext(UserContext)
-
+  console.log(user)
   return (
     <div className="w-screen h-screen bg-pallette4">
       <Header />
-      <Welcome />
-      <CardList />
+      <Routes>
+        <Route path="/" element={!user ? <Welcome /> : <CardList />} />
+      </Routes>
     </div>
   );
 }
