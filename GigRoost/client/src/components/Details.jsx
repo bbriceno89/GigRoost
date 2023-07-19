74ae510abe7728f2ccd5ba518ffc57026d6409b3
bbriceno89 <127133827+bbriceno89@users.noreply.github.com>
2023-07-19 13:29:32 -0600

    details component bones

--- GigRoost/client/src/components/Details.jsx
+++ GigRoost/client/src/components/Details.jsx
@@ -1,9 +1,27 @@
-function Details(){
-    return(
-        <div>
-            <h1>Details</h1>
-        </div>
-    )
-}
+import React from 'react';
+
+function Details() {
+
+    return (
+    <div>
+      <div className="Available Roosts">
+        {/* Availability Calendar or Date Selection Component */}
+        <h2>Select Dates:</h2>
+        {/* Include your date selection component here */}
+        {/* e.g., <CalendarComponent /> */}
+        <button>Check Availability</button>
+      </div>
+      <div className="image-container">
+        <img src="apartment-image.jpg" alt="Apartment" />
+      </div>
+      <div className="description">
+        <h2>Apartment Name/Number</h2>
+        <p>
+          [Apartment Description]
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Details;
