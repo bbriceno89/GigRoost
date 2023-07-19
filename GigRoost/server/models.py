ac33a10e6ead564036fed752bfdd45fe871903ba
tgartland89 <thomas.gartland@gmail.com>
2023-07-19 10:38:29 -0600

    updated rental models with to_dict- GET is working

--- GigRoost/server/models.py
+++ GigRoost/server/models.py
@@ -95,3 +95,22 @@ class Rental(db.Model, SerializerMixin):
 
 # Establish a relationship between User and Rental
     host = db.relationship("User", backref="rentals")
+
+    def to_dict(self):
+        rental_data = {
+            'rental_id': self.rental_id,
+            'location': self.location,
+            'beds': self.beds,
+            'baths': self.baths,
+            'sq_ft': self.sq_ft,
+            'description': self.description,
+            'image_url': self.image_url,
+            'availability_dates': self.availability_dates,
+            # Exclude circular references, for example:
+            # 'host': self.host.to_dict() if self.host else None,
+            # 'reviews': [review.to_dict() for review in self.reviews],
+            # 'artist_bookings': [booking.to_dict() for booking in self.artist_bookings],
+            # Add other attributes as needed
+        }
+        return rental_data
+
