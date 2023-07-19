6837831c5c64a4f30100cae54586a1d362b8af2f
tgartland89 <thomas.gartland@gmail.com>
2023-07-19 15:32:36 -0600

    seed.py and CardList.jsx notes updated

--- GigRoost/client/src/components/CardList.jsx
+++ GigRoost/client/src/components/CardList.jsx
@@ -1,51 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 
 const CardList = () => {
   const [rentalItems, setRentalItems] = useState([]);
 
   useEffect(() => {
-    // Fetch data from the server
-    axios.get('http://127.0.0.1:5555/rentals')
-      .then((response) => {
-        setRentalItems(response.data);
+    // Fetch rental items from the server
+    fetch('http://127.0.0.1:5555/rentals?limit=8')
+      .then((response) => response.json())
+      .then((data) => {
+        const first8Items = data.slice(0,8);
+        setRentalItems(first8Items);
       })
       .catch((error) => {
         console.error('Error fetching rental data:', error);
       });
   }, []);
-
-  const firstEightItems = rentalItems.slice(0, 8);
-
-  // Function to fetch a random image from Unsplash
-  const fetchRandomImage = async () => {
-    try {
-      const response = await axios.get('https://source.unsplash.com/featured/?house,apartment');
-      return response.request.responseURL;
-    } catch (error) {
-      console.error('Error fetching random image:', error);
-      return null;
-    }
-  };
-
-  // Function to assign a random image URL to each rental item
-  const assignRandomImages = async () => {
-    const itemsWithRandomImages = await Promise.all(
-      firstEightItems.map(async (item) => {
-        const randomImageUrl = await fetchRandomImage();
-        return {
-          ...item,
-          randomImageUrl,
-        };
-      })
-    );
-    setRentalItems(itemsWithRandomImages);
-  };
-
-  useEffect(() => {
-    assignRandomImages();
-  }, [firstEightItems]); // Run the assignment whenever firstEightItems changes
-
+  
   return (
     <div className="bg-pallette1 w-fill h-fit mx-20 my-20">
       <h2 className="text-pallette6 text-3xl font-bold text-center pt-12">
@@ -55,7 +25,7 @@ const CardList = () => {
         {rentalItems.map((item) => (
           <div key={item.rental_id} className="bg-white p-2 shadow-md">
             <img
-              src={item.randomImageUrl || '/fallback-image.jpg'}
+              src={item.image_url || '/fallback-image.jpg'}
               alt={`Rental Item ${item.rental_id}`}
               className="w-full h-36 object-cover mb-2"
             />
