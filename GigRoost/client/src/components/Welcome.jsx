ebed0ffffe9c8b1cdcb3981431fcb6ad7c5a43f0
tgartland89 <thomas.gartland@gmail.com>
2023-07-20 16:12:07 -0600

    set text back to white for welcome

--- GigRoost/client/src/components/Welcome.jsx
+++ GigRoost/client/src/components/Welcome.jsx
@@ -4,7 +4,7 @@ import chickenHoodGuitar from "../assets/chicken-hood-guitar.gif";
 function Welcome() {
   return (
     <div className="bg-pallette1 rounded-xl w-fill h-fit mx-20 my-20 p-8">
-      <h2 className="text-black text-3xl font-bold text-center py-4">
+      <h2 className="text-pallette6 text-3xl font-bold text-center py-4">
         Welcome to GigRoost
       </h2>
       <img
@@ -12,7 +12,7 @@ function Welcome() {
         alt="Chicken with a Hood Playing Guitar"
         className="mx-auto"
       />
-      <div className="text-black font-semibold text-center mx-8 pb-4">
+      <div className="text-pallette6 font-semibold text-center mx-8 pb-4">
         <p>GigRoost makes touring cheap and easy!</p>
         <p>Find a place to stay near your venue, or offer your place in exchange for tickets!</p>
         <p>With GigRoost, you are helping to bring together the music community!</p>
