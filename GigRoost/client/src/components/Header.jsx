d49590cec82887a085d5e32c1dba2a3665bd6241
tgartland89 <thomas.gartland@gmail.com>
2023-07-20 16:03:46 -0600

    added gif to welcome page

--- GigRoost/client/src/components/Header.jsx
+++ GigRoost/client/src/components/Header.jsx
@@ -4,10 +4,10 @@ import logo from "../assets/chicken-coop-icon-hen-house-png-image.png";
 
 function Header() {
   return (
-    <div className="bg-pallette1 px-2 py-4 flex items-center justify-between">
+    <div className="bg-pallette1 px-4 py-2 rounded-lg flex items-center justify-between">
       <div className="flex items-center ml-auto pr-10">
         <h1 className="text-pallette6 text-3xl font-bold mr-4">GigRoost</h1>
-        <div className="bg-gray-700 p-0 rounded-full overflow-hidden"> {/* Use the default bg-gray-800 class */}
+        <div className="bg-gray-700 p-0 rounded-full overflow-hidden"> 
           <img src={logo} alt="Chicken Logo" className="w-16 h-16 object-contain aspect-w-1 aspect-h-1" />
         </div>
       </div>
--- GigRoost/client/src/components/Welcome.jsx
+++ GigRoost/client/src/components/Welcome.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import Login from "./Login";
-import Signup from "./Signup";
+import chickenHoodGuitar from "../assets/chicken-hood-guitar.gif";
 
 function Welcome() {
   return (
-    <div 
-    className=" bg-pallette1 rounded-lg
-    w-fill h-fit 
-    mx-20 my-20">
-        <h2 className="text-pallette6 text-3xl font-bold text-center py-12">
-            Welcome to GigRoost
-        </h2>
-        <p className="text-pallette6 font-semibold text-center mx-16 pb-12">GigRoost makes touring cheap and easy! Find a place to stay near your venue, or offer your place in exchange for tickets! With GigRoost, you are helping to bring together the music community!</p>
+    <div className="bg-pallette1 rounded-xl w-fill h-fit mx-20 my-20 p-8">
+      <h2 className="text-black text-3xl font-bold text-center py-4">
+        Welcome to GigRoost
+      </h2>
+      <img
+        src={chickenHoodGuitar}
+        alt="Chicken with a Hood Playing Guitar"
+        className="mx-auto"
+      />
+      <div className="text-black font-semibold text-center mx-8 pb-4">
+        <p>GigRoost makes touring cheap and easy!</p>
+        <p>Find a place to stay near your venue, or offer your place in exchange for tickets!</p>
+        <p>With GigRoost, you are helping to bring together the music community!</p>
+      </div>
     </div>
   );
 }
