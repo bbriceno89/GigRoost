2c5a88d78309f6df5bee776f38a43187936d05bb
tgartland89 <thomas.gartland@gmail.com>
2023-07-20 15:17:58 -0600

    added logo to site

--- GigRoost/client/src/components/Header.jsx
+++ GigRoost/client/src/components/Header.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import NavBar from "./NavBar";
+import logo from "../assets/chicken-coop-icon-hen-house-png-image.png"; 
 
 function Header() {
   return (
-    <>
-      <h1 
-      className="bg-pallette1 
-      text-left 
-      text-pallette6 
-      text-3xl 
-      font-bold 
-      w-screen h-14 
-      px-2 py-4">
-        GigRoost
-      </h1>
+    <div className="bg-pallette1 px-2 py-4 flex items-center justify-between">
+      <div className="flex items-center ml-auto pr-10">
+        <h1 className="text-pallette6 text-3xl font-bold mr-4">GigRoost</h1>
+        <div className="bg-gray-700 p-0 rounded-full overflow-hidden"> {/* Use the default bg-gray-800 class */}
+          <img src={logo} alt="Chicken Logo" className="w-16 h-16 object-contain aspect-w-1 aspect-h-1" />
+        </div>
+      </div>
       <NavBar />
-    </>
+    </div>
   );
 }
-
 export default Header;
