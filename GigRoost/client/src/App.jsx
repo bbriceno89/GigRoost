f6ccb8b15215a57e4659a59bf7c30c75aaafef87
bbriceno89 <127133827+bbriceno89@users.noreply.github.com>
2023-07-20 10:18:49 -0600

    commit pull request
    GigRoost/client/src/App.jsx

--- GigRoost/client/src/App.jsx
+++ GigRoost/client/src/App.jsx
@@ -2,15 +2,10 @@ import { useContext, useState } from "react";
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import CardList from "./components/CardList"; 
-<<<<<<< HEAD
 import Details from "./components/Details";
 
 function App() {
   const [user, setUser] = useState(null);
-=======
-import { UserContext } from "./components/context/UserContext";
-import { Router, Routes, Route } from "react-router-dom";
->>>>>>> main
 
 function App() {
   const { user, setUser } = useContext(UserContext)
@@ -18,18 +13,12 @@ function App() {
   return (
     <div className="w-screen h-screen bg-pallette4">
       <Header />
-<<<<<<< HEAD
       <Welcome />
       <CardList /> {/* Add the CardList component here */}
       <Details /> {/* Add the Details component here */}
       
-=======
-      <Routes>
-        <Route path="/" element={!user ? <Welcome /> : <CardList />} />
-      </Routes>
->>>>>>> main
     </div>
   );
 }
-
+}
 export default App;
