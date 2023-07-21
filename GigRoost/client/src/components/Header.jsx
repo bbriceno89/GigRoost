4f1b89ab7371d66051a6fd0949d57013a17aaa43
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-21 09:39:21 -0600

    add return to home screen on click

--- GigRoost/client/src/components/Header.jsx
+++ GigRoost/client/src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate()
+
+  function handleReturn() {
+    navigate('/', {replace: true})
+  }
+
   return (
     <>
-      <h1 
+      <h1 onClick={handleReturn}
       className="bg-pallette1 
       text-left 
       text-pallette6 
       text-3xl 
       font-bold 
       w-screen h-14 
-      px-2 py-4">
+      px-2 py-4 cursor-pointer">
         GigRoost
       </h1>
       <NavBar />
