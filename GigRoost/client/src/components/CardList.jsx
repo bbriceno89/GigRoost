cd906112a9794c85fecde4f4a286020359830fdc
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-21 09:43:36 -0600

    hide scrollbar

--- GigRoost/client/src/components/CardList.jsx
+++ GigRoost/client/src/components/CardList.jsx
@@ -17,8 +17,8 @@ const CardList = () => {
   }, []);
   
   return (
-    <div className="bg-pallette1 w-fill h-4/5 mx-7 my-5 overflow-y-scroll">
-      <h2 className="text-pallette6 text-3xl font-bold text-center pt-5">
+    <div className="bg-pallette1 w-fill h-4/5 mx-7 my-5 overflow-y-scroll no-scrollbar">
+      <h2 className="text-pallette6 text-3xl font-bold text-center pt-5 bg-pallette1">
         Find a Place to Crash
       </h2>
       <div className="grid gap-4 grid-cols-4 py-7 px-3">
