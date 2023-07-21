a62e4bf9fd8d7b382952020fd910597d9a7773de
tgartland89 <thomas.gartland@gmail.com>
2023-07-21 09:53:35 -0600

    working on front end

--- GigRoost/client/src/components/Card.jsx
+++ GigRoost/client/src/components/Card.jsx
@@ -1,31 +1,9 @@
 import React from "react";
 
-
-
 function Card({ item }) {
-  const handleClick = () => {
-    window.location.href = `/rentals/${item.rental_id}`;
-  };
 
+    const desc_preview = item.description.slice(0, 64) + "..."
 
-  const desc_preview = item.description.slice(0, 64) + "...";
-
-  return (
-    <div key={item.rental_id} className="bg-white p-2 shadow-md" onClick={handleClick}>
-      <img
-        src={item.image_url || "/fallback-image.jpg"}
-        alt={`Rental Item ${item.rental_id}`}
-        className="w-full h-fit object-cover mb-2"
-      />
-
-      <p
-        className="text-base text-left"
-        style={{ color: "black", fontWeight: "bold" }}
-      >
-        {desc_preview}
-      </p>
-    </div>
-  );
     return(
         <div className="overflow-hidden rounded-xl relative h-72 shadow-lg group">
           <img
--- GigRoost/client/src/components/Signup.jsx
+++ GigRoost/client/src/components/Signup.jsx
@@ -1,41 +1,80 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { UserContext } from "./context/UserContext";
+import { useNavigate } from "react-router-dom";
 
-function NewRental() {
-    const [formData, setFormData] = useState({})
+function Signup() {
+  const [signupData, setSignupData] = useState({})
+  const { user, setUser } = useContext(UserContext)
+  const [isError, setIsError] = useState(false)
 
-    function handleChange(e) {
-        const name = e.target.name;
-        const value = e.target.value;
-        setFormData({...formData, [name]: value})
-    }
+  const navigate = useNavigate()
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formData)
-    }
+  function handleType(e) {
+    setSignupData({...signupData, account_type: e.target.value})
+  }
 
-    return(
-        <div className="bg-pallette1 w-fill h-fit py-6">
-            <p className="text-center text-lg font-semibold text-pallette6 mb-5">New Listing</p>
-            <form className="grid grid-cols-5 grid-rows-12 gap-y-1 gap-x-3 outline-double outline-8 outline-pallette4 bg-pallette2 py-3" >
-                <span className="col-start-2">Location:</span>
-                <input name="location" className="col-start-2 col-span-3" />
-                <span className="col-start-2">Beds:</span>
-                <span className="col-start-3">Baths:</span>
-                <span className="col-start-4">Square Feet:</span>
-                <input name="beds" type="number" className="col-start-2" />
-                <input name="baths" type="number" step="0.5" className="col-start-3" />
-                <input name="sq_ft" className="col-start-4" />
-                <span className="col-start-2">Image:</span>
-                <input name="image_url" placeholder="Image URL" className="col-start-2 col-span-2" />
-                <span className="col-start-2">Description:</span>
-                <input name="description"className="col-start-2 col-span-3"/>
-                <button type="submit" className="col-start-4 bg-pallette5 hover:bg-pallette3 outline outline-pallette4 outline-1">Submit</button>
-            </form>
+  function handleChange(e) {
+    const name = e.target.name;
+    const value= e.target.value;
+    setSignupData({...signupData, [name]: value})
+  }
+  console.log(signupData)
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(signupData)
+    })
+    .then(r=>{
+      if (r.ok) {
+        setIsError(false)
+        return r.json()}
+      throw new Error("Account name already taken")
+    })
+    .then(data=>{
+      setUser(data)
+      navigate('/', {replace: true})
+    })
+    .catch((error)=>{
+      console.log(error)
+      setIsError(true)
+    })
+  }
+
+  const errorMessage = (
+    <div className="grid grid-cols-3 gap-4">
+      <p className="col-start-2 w-fit outline outline-red-500 p-1 mb-4 text-sm text-red-600 bg-red-300">Error: Username already taken</p>
+    </div>
+  )
+
+
+  return (
+    <>
+    <div 
+    className=" bg-pallette1 rounded-lg
+    w-fill h-fit 
+    mx-20 my-20 pb-12">
+      <h2 className="text-pallette6 text-3xl font-bold text-center py-12">
+          Welcome to GigRoost
+      </h2>
+      {!!isError ? errorMessage : null}
+      <form onChange={handleChange} onSubmit={handleSubmit} className="grid grid-rows-4 grid-cols-3 gap-4 h-36">
+        <input placeholder="Username" name="username" type="text" className=" col-start-2 rounded-md"/>
+        <input placeholder="Password" name="password" type="text" className=" col-start-2 rounded-md"/>
+        <div className="col-start-2">
+          <div className="grid grid-cols-2 gap-2">
+            <button value="host" type='button' onClick={handleType} className="col-start-1 bg-pallette5 focus:bg-pallette3">Host</button>
+            <button value="artist" type="button" onClick={handleType} className="col-start-2 bg-pallette5 focus:bg-pallette3">Artist</button>
+          </div>
         </div>
-
-
-    )
+        <button type="submit" className=" col-start-2 rounded-md bg-pallette5">Create Account</button>
+      </form>
+    </div>
+    </>
+  );
 }
 
-export default NewRental;
+export default Signup;
