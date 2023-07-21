0aa843a60c2d649bfcaa63714270c359cf490b06
tgartland89 <thomas.gartland@gmail.com>
2023-07-21 12:46:53 -0600

    updating logo

--- GigRoost/client/src/components/Header.jsx
+++ GigRoost/client/src/components/Header.jsx
@@ -4,19 +4,26 @@ import logo from "../assets/chicken-coop-icon-hen-house-png-image.png";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function handleReturn() {
-    navigate('/', {replace: true})
+    navigate('/', {replace: true});
   }
 
   return (
     <div className="bg-pallette1 px-4 py-2 rounded-lg flex items-center justify-between">
-      <div className="flex items-center ml-auto pr-10">
-        <h1 onClick ={handleReturn} className="text-pallette6 text-3xl font-bold mr-4">GigRoost</h1>
-        <div className="bg-gray-700 p-0 rounded-full overflow-hidden"> 
-          <img src={logo} alt="Chicken Logo" className="w-32 h-32 object-contain aspect-w-1 aspect-h-1" />
-        </div>
+      <h1
+        onClick={handleReturn}
+        className="text-pallette6 text-3xl font-bold mr-4 cursor-pointer"
+      >
+        GigRoost
+      </h1>
+      <div className="rounded-full overflow-hidden w-16 h-11 pr-2">
+        <img
+          src={logo}
+          alt="Chicken Logo"
+          className="w-full h-full object-contain"
+        />
       </div>
       <NavBar />
     </div>
