1d3789b1216cafd79c3c238b3323880e8a17690e
tgartland89 <thomas.gartland@gmail.com>
2023-07-21 11:40:06 -0600

    added MyRental and route for CardList and Detail

--- GigRoost/client/src/components/MyRentals.jsx
+++ GigRoost/client/src/components/MyRentals.jsx
@@ -1,39 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Card from './Card';
-import { Link } from "react-router-dom"; // Import Link
+import { UserContext } from './context/UserContext';
 
-const CardList = () => {
+const MyRentals = () => {
   const [rentalItems, setRentalItems] = useState([]);
+  const { user, setUser } = useContext(UserContext)
 
   useEffect(() => {
     // Fetch rental items from the server
-    fetch('/api/rentals?limit=8')
+    fetch('/api/rentals')
       .then((response) => response.json())
       .then((data) => {
-        setRentalItems(data);
+        setRentalItems(data)
       })
       .catch((error) => {
         console.error('Error fetching rental data:', error);
       });
   }, []);
- 
   return (
     <div className="bg-pallette1 w-fill h-4/5 mx-7 my-5 overflow-y-scroll no-scrollbar">
       <h2 className="text-pallette6 text-3xl font-bold text-center pt-5 bg-pallette1">
-        Find a Place to Crash
+        My Listings
       </h2>
       <div className="grid gap-4 grid-cols-4 py-7 px-3">
-        {rentalItems.map((item) => (
-          <Link key={item.rental_id} to="/details"> {/* Use Link to navigate to the details page */}
-            <Card
-              item={item}
-              onClick={() => onRentalClick(item.rental_id)}
-            />
-          </Link>
-        ))}
+        {rentalItems.map((item) => <Card item={item} key={item.rental_id}/> )}
       </div>
     </div>
   );
 };
 
-export default CardList;
+export default MyRentals;
