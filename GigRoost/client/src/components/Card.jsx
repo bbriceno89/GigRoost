4b3df55e9e1a1a4153efdcf8c9a13791ae508c8c
Jacob Ives <jacob.w.ives@gmail.com>
2023-07-20 19:03:00 -0600

    card text positioning functions properly at all window sizes

--- GigRoost/client/src/components/Card.jsx
+++ GigRoost/client/src/components/Card.jsx
@@ -11,9 +11,9 @@ function Card({ item }) {
             alt={`Rental Item ${item.rental_id}`}
             className="w-full h-full object-cover transition-transform group-hover:scale-110 duration-200"
           />
-          <div className="p-4 absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-pallette4/95 to-transparent translate-y-[25%] group-hover:translate-y-0 transition-all">
+          <div className="p-4 absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-pallette4/95 to-transparent translate-y-1/4 group-hover:translate-y-0 transition-all">
             <h2 className="text-pallette6 text-xl font-semibold" >{item.location}</h2>
-            <p className="text-pallette5 text-sm">{desc_preview}</p>
+            <p className="text-pallette5 text-sm w-40">{desc_preview}</p>
           </div>
         </div>
     )
