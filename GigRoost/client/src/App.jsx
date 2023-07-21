d4e3e9f3392222f5d25e50fcf6ca3717a09e6d8e
tgartland89 <thomas.gartland@gmail.com>
2023-07-20 19:39:07 -0600

    updated components

--- GigRoost/client/src/App.jsx
+++ GigRoost/client/src/App.jsx
@@ -2,10 +2,12 @@ import { useContext, useEffect, useState } from "react";
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import CardList from "./components/CardList"; 
+import Details from "./components/Details";
 import { UserContext } from "./components/context/UserContext";
 import { Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import NewRental from "./components/NewRental";
 
 function App() {
   const { user, setUser } = useContext(UserContext)
@@ -27,6 +29,7 @@ function App() {
         <Route path="/" element={!user ? <Welcome /> : <CardList />} />
         <Route path='/login' element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
+        <Route path="/rentals/new" element={<NewRental />} />
       </Routes>
     </div>
   );
